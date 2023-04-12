ae06c24b8dcdaef282dc5d8aa4031abc5981605f
Nilamjr <nilamrj07@gmail.com>
2023-04-12 06:48:16 +0530

    forloop practice programms

--- Js Course With HarryBhai/Practice/09_forloop.js
+++ Js Course With HarryBhai/Practice/09_forloop.js
@@ -5,12 +5,50 @@ let n = 3;
 n = Number.parseInt(n);
 
 for(let i=1; i<=n; i++){
-    console.log("Before sum>>>>>", sum);
-    console.log("Before i", i);
-    console.log("sum + i",sum, "+" + i, sum += i);      
-    console.log("After i",i);
-    console.log("After sum>>>",sum);
-    // sum += i;
-    console.log("sum of first" + n + "natural number is" + sum);
+ 
+    // console.log("before i",i);
+    // i=1
+    // i=2
+    // i=3
+   
+    // console.log("before sum>>>",sum);
+    // sum= 0
+    // sum= 1
+    // sum= 3
+   
+    // console.log("sum + i",sum, "+" + i, sum += i);  
 
+    // 0+1=1
+    // 1+2=3
+    // 3+3=6
+    
+    // console.log("After i",i);
+    // i=1
+    // i=2
+    // i=3
+    
+
+    
+    // console.log("After sum>>>",sum);
+    // sum= 1
+    // sum= 3
+    // sum= 6
+    
+    // console.log("sum of first" + n + "natural number is" + sum);
+    // sum 0f first 3 natural number is sum = 1
+    // sum 0f first 3 natural number is sum = 3
+    // sum 0f first 3 natural number is sum = 6
 }
+
+for(let i=0; i<n; i++){
+    console.log("sum",sum);
+    console.log(sum += (i+1));
+    // console.log("sum",sum);
+    // console.log(sum+=i);
+    // console.log("sum after", sum);
+}
+
+
+// Write program of factorial here
+// print from 1 number to other 
+
---
+++ Js Course With HarryBhai/Practice/practiceSet2.js
@@ -0,0 +1,57 @@
+// chapter Two Pracitce Set 
+
+// Problem 1 
+
+// let age = 10;
+
+// if(age > 10 && age < 20){
+//     console.log("Your age is lies between 10 and 20");
+// }else{
+//     console.log("Your age does'nt lies between 10 and 20");
+// }
+
+
+// Problem 2
+// let age = 12;
+// age = Number.parseInt(age);
+
+// switch (age){
+//     case '12':
+//     console.log("Your age is 12")
+//     break
+//     case '23':
+//     console.log("Your age is 23");
+//     break
+//     default:
+//     console.log("Your age is not lies between any of above");
+// }
+
+// Problem 3 
+// let num = 24;
+
+//  num  = Number.parseInt(num);
+
+//  if(num % 2 == 0 && num % 3 == 0){
+//     console.log("Your Number is divisible by 2 and 3");
+//  }else{
+//     console.log("Your number is not divisible by 2 and 3");
+//  }
+
+// Problem 4 
+
+// let num1 = 55;
+// num1 = Number.parseInt(num1);
+
+// if(num1 % 2 == 0){
+//     console.log("Your number is divisible by 2");
+// }else if(num1 % 3 == 0){
+//     console.log("Your Numbar is divisible by 3");
+// }else{
+//     console.log("Your number is not divisible by 2 or 3");
+// }
+
+// Problem 5 
+let age = 10;
+let a = age > 18 ? "You can drive": "You can not drive";
+
+console.log(a);
