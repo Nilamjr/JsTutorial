b5400488b15683751710fdcf4d871b7cf957281c
Nilamjr <nilamrj07@gmail.com>
2023-04-16 15:22:13 +0530

    practice set and strings

--- Js Course With HarryBhai/Practice/12_Praciceset3.js
+++ Js Course With HarryBhai/Practice/12_Praciceset3.js
@@ -1,12 +1,46 @@
 // Question 1 
 
-let marks = {
+const marks = {
     nilam: 96,
     khushal: 89,
     jay: 50,
     Nir: 40
 }
 
-for(let i = 0; i < Object.keys(marks).length; i++){
-    console.log("The marks of" + Object.keys(marks)[i] + "are" +  marks(Object.keys(marks)[i]));
+// for(let i = 0; i < Object.keys(marks).length; i++){
+//     console.log("The marks of" + Object.keys(marks)[i] + "are" +  marks(Object.keys(marks)[i]));
+// }
+
+for(let i = 0; i < marks.length; i++){
+//  console.log("new");   
 }
+
+for(let i = 0; i < Object.keys(marks).length; i++){
+    // console.log("The marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]]);
+}
+
+
+// Question 2
+
+for(let key in marks){
+    // console.log(key);
+    console.log("Marks of " + key + " are " + marks[key]); 
+}
+
+// Question 3 
+// try to run this in console because prompt is browser method 
+// let cn = 5;
+// let i;
+// while( i != cn){
+//     i = prompt('Enter a Number');
+    // console.log("try again");
+// }
+// console.log("YOu have entered a correct number");
+
+// Question 4 
+
+const mean = (a, b, c, d) => {
+    return (a+b+c+d)/4
+}
+// console.log(2, 4, 3, 6);
+console.log(mean(2,4,5,7));
---
+++ Js Course With HarryBhai/Practice/13_strings.js
@@ -0,0 +1,36 @@
+let myName = "Nilam";
+let myFriendName = 'Divya';
+
+// console.log("My Name is", myName);
+// console.log("My Friend's name is", myFriendName);
+
+// below program is the example of String Interpolation which means template literal 
+
+let girl1 = 'Divya';
+let girl2 = 'Krishna';
+
+// console.log(`${girl1} is my friend and ${girl2} is my sister`);
+
+// Escape sequence character 
+// which behave as a single character while it is two different character 
+
+// let fruit = 'grap'es';
+let fruit = 'grap\'es';
+let fruit1 = "grap\"es";
+// console.log(fruit);
+console.log(fruit1);
+// console.log(fruit.length);
+
+let veges = 'potat\no';
+// console.log(veges);
+// console.log("length of veges",veges.length);
+
+let fastFood = 'bur\rger';
+// console.log("length of fastfood",fastFood.length);
+console.log("\n");
+// console.log("length of fastfood",fastFood);
+console.log("length of fastfood",fastFood);
+// console.log(fastFood);
+// console.log(fastFood);
+
+
