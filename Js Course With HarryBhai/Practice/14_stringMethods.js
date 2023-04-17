36b7716974818a97c9f5e36e14904e20e730f195
Nilamjr <nilamrj07@gmail.com>
2023-04-17 07:01:10 +0530

    string method

---
+++ Js Course With HarryBhai/Practice/14_stringMethods.js
@@ -0,0 +1,20 @@
+// let myName = "Nilam\"";
+// let myName = "Nilam____________________________________";
+let myName = "Nilam";
+let friend = "Khushal";
+
+// Properties and Methods 
+
+// console.log(myName.length);
+// console.log(myName.toUpperCase());
+// console.log(myName.toLowerCase());
+// console.table(myName.slice(2));
+// console.log(myName.slice(2, 4));
+// console.log(myName.replace("Nil", "khush"));
+// console.log(myName.concat(" is a friend of ", friend));
+
+// let newFriend = "           Khush    ";
+// console.log(newFriend);
+// console.log(newFriend.trim());
+
+// let fri = 
