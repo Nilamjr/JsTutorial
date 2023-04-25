acced5b97cb398ec5455946e9447901838d2f5f3
Nilamjr <nilamrj07@gmail.com>
2023-04-25 06:27:49 +0530

    Console objects

---
+++ Js Course With HarryBhai/Intermediate/25_consoleObjects.js
@@ -0,0 +1,96 @@
+console.log("Hey kanha, are you watching me na?");
+
+console.log(console);
+// console.error("Its an error");
+
+// console.assert(5>55); error if condition is false
+
+console.assert(5<555);
+
+// console.clear(); clears all logs
+
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+
+// console.table(obj);
+
+let arr = [1,2,34,5,67,8,9]
+
+// console.log(arr);
+// console.table(arr);
+// console.warn("Hey keep your mind in this program and javascrip");
+console.info("Its an important info");
+
+
+
+console.log("log")
+console.info("info")
+console.warn("warn")
+console.error("error")
+console.log("err")
+console.log("err" != false)
+console.log("err" == false)
+console.time("a")
+console.timeEnd("a")
+
+console.table(console)
+
+
+// console.time("Forloop")
+
+// for(let i=0; i<500; i++){
+//     console.log("Radha krishn")
+// }
+
+// console.timeEnd("Forloop")
+
+// console.time("Whileloop")
+
+// let i = 0
+// while(i < 500){
+//     console.log("Radha krishn")
+//     i++
+// }
+// console.timeEnd("Whileloop")
+
+// console.time("Forloop")
+
+// for(let i=0; i<500; i++){
+//     console.log("233")
+// }
+
+// console.timeEnd("Forloop")
+
+// console.time("Whileloop")
+
+// let i = 0
+// while(i < 500){
+//     console.log("233")
+//     i++
+// }
+// console.timeEnd("Whileloop")
+
+
+console.time("Forloop")
+
+for(let i=0; i<5; i++){
+    console.log(233)
+}
+
+console.timeEnd("Forloop")
+
+console.time("Whileloop")
+
+let i = 0
+while(i < 5){
+    console.log(233)
+    i++
+}
+console.timeEnd("Whileloop")
+
+
+console.clear();
+
