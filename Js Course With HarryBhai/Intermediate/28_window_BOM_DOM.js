0aa098486b6a89c090b857f1a7741affff37176b
Nilamjr <nilamrj07@gmail.com>
2023-04-27 06:26:59 +0530

    remove comments

--- Js Course With HarryBhai/Intermediate/28_window_BOM_DOM.js
+++ Js Course With HarryBhai/Intermediate/28_window_BOM_DOM.js
@@ -1,5 +1,5 @@
-// console.log(window);
+console.log(window);
 
-// window.console.log(document) //window is a global object
+window.console.log(document) //window is a global object
 
 // console.log(location.href = "https://codewithharry.com");
