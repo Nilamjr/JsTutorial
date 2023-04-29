ef97de902399e0f781f4ccfe7cef758da52ca2e0
Nilamjr <nilamrj07@gmail.com>
2023-04-29 06:48:54 +0530

    trying to solve exercise 1

--- Js Course With HarryBhai/Practice/22_Exercise1.js
+++ Js Course With HarryBhai/Practice/22_Exercise1.js
@@ -1,18 +1,37 @@
 let randomNumber = Math.floor(Math.random() * 100);
+
 console.log(randomNumber);
+// console.log(typeof randomNumber);
 
-let guessCounter = 0;
-let chances = 100;
+// let guessCounter = 0;
+// let chances = 100;
 
-let EnteredNum = prompt("Enter a Number between 1 to 100");
 
-if(EnteredNum == randomNumber){
-    console.log("You are a winner");
-    console.log("In If")
-}else{
-    console.log("Enter again");
-    EnteredNum = prompt("Enter number again");
-    chances -= 100
-    console.log("Your remain chances are", chances);
-    console.log("In else")
-}   
+console.log(typeof enteredNum);
+console.log(enteredNum)
+
+while(enteredNum === randomNumber){
+    let enteredNum = prompt("Enter a Number between 1 to 100");
+    enteredNum = Number.parseInt(enteredNum);   
+
+    if(enteredNum < randomNumber){
+        console.log("Guessed Number is smaller than Actual Number");
+    }else if(enteredNum > randomNumber ){
+        console.log("Guessed Number is greater than Actual Number");
+    }else{
+        alert("You wno the game");
+        console.log("You won the game");
+    }    
+}
+
+
+// if(EnteredNum === randomNumber){
+//     console.log("You are a winner");
+//     console.log("In If")
+// }else{
+//     console.log("Enter again");
+//     EnteredNum = prompt("Enter number again");
+//     chances -= 100
+//     console.log("Your remain chances are", chances);
+//     console.log("In else")
+// }   
