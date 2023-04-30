def856cbe9068fc65dfadb5614b9b5e924b773d5
Nilamjr <nilamrj07@gmail.com>
2023-04-30 06:54:59 +0530

    Walking the DOM

---
+++ Js Course With HarryBhai/Intermediate/31_WalkingTheDom.js
@@ -0,0 +1,9 @@
+
+console.log("Its a Document Head",document.head);
+console.log("typeof Head",typeof document.head);
+console.log("Its a Document Element",document.documentElement);
+console.log("typeof Document Element",typeof document.documentElement);
+
+
+console.log("Title is :::", document.title)
+console.log("typeof Title is",typeof document.title)
--- Js Course With HarryBhai/Practice/22_Exercise1.js
+++ Js Course With HarryBhai/Practice/22_Exercise1.js
@@ -7,23 +7,22 @@ console.log(randomNumber);
 // let chances = 100;
 
 
-console.log(typeof enteredNum);
+let enteredNum = prompt("Enter a Number between 1 to 100");
+enteredNum = Number.parseInt(enteredNum);   
 console.log(enteredNum)
+console.log(typeof enteredNum);
 
-while(enteredNum === randomNumber){
-    let enteredNum = prompt("Enter a Number between 1 to 100");
-    enteredNum = Number.parseInt(enteredNum);   
-
-    if(enteredNum < randomNumber){
-        console.log("Guessed Number is smaller than Actual Number");
-    }else if(enteredNum > randomNumber ){
-        console.log("Guessed Number is greater than Actual Number");
-    }else{
-        alert("You wno the game");
-        console.log("You won the game");
-    }    
-}
-
+// while(enteredNum !== randomNumber){
+//     if(enteredNum < randomNumber){
+//         alert("Guessed Number is smaller than Actual Number");
+//     }else if(enteredNum > randomNumber ){
+//         alert("Guessed Number is greater than Actual Number");
+//     }else{
+//         alert("You won the game");
+//         break;
+//     }
+//     enteredNum = prompt("Enter a Number between 1 to 100");    
+// }
 
 // if(EnteredNum === randomNumber){
 //     console.log("You are a winner");
