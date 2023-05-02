4420d60f5d9b411bb9ef98bd02c4d49a9b934fb9
Nilamjr <nilamrj07@gmail.com>
2023-05-02 06:27:17 +0530

    start to learn parent object

--- Js Course With HarryBhai/Practice/22_Exercise1.js
+++ Js Course With HarryBhai/Practice/22_Exercise1.js
@@ -6,23 +6,22 @@ console.log(randomNumber);
 // let guessCounter = 0;
 // let chances = 100;
 
-
 let enteredNum = prompt("Enter a Number between 1 to 100");
 enteredNum = Number.parseInt(enteredNum);   
 console.log(enteredNum)
 console.log(typeof enteredNum);
 
-// while(enteredNum !== randomNumber){
-//     if(enteredNum < randomNumber){
-//         alert("Guessed Number is smaller than Actual Number");
-//     }else if(enteredNum > randomNumber ){
-//         alert("Guessed Number is greater than Actual Number");
-//     }else{
-//         alert("You won the game");
-//         break;
-//     }
-//     enteredNum = prompt("Enter a Number between 1 to 100");    
-// }
+while(enteredNum !== randomNumber){
+    if(enteredNum < randomNumber){
+        alert("Guessed Number is smaller than Actual Number");
+    }else if(enteredNum > randomNumber ){
+        alert("Guessed Number is greater than Actual Number");
+    }else{
+        alert("You won the game");
+        break;
+    }
+    enteredNum = prompt("Enter a Number between 1 to 100");    
+}
 
 // if(EnteredNum === randomNumber){
 //     console.log("You are a winner");
@@ -33,4 +32,4 @@ console.log(typeof enteredNum);
 //     chances -= 100
 //     console.log("Your remain chances are", chances);
 //     console.log("In else")
-// }   
+// }
