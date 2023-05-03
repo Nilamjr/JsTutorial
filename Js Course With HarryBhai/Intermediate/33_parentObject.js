ebb4fb6036a04d22a0fd96b3e2823c33e8ee1a6d
Nilamjr <nilamrj07@gmail.com>
2023-05-03 20:21:12 +0530

    ElementOnlyNavigation

---
+++ Js Course With HarryBhai/Intermediate/33_parentObject.js
@@ -0,0 +1,18 @@
+console.log("Learning Parent and siblings")
+
+// There is $0 which refere selected element in cDeveloper Tools 
+// $0
+// $1
+// $0.childNodes
+
+// console.log("Document Element",document.documentElement);
+// console.log("Document Element parentNode",document.documentElement.parentNode);
+// console.log("Document Element parentElement",document.documentElement.parentElement);
+
+console.log(document.body.firstChild)
+let a = document.body.firstChild
+
+console.log(a.parentNode)
+console.log(a.parentElement)
+
+console.log(a.firstChild.nextSibling);
---
+++ Js Course With HarryBhai/Intermediate/34_ElementOnlyNavigation.js
@@ -0,0 +1,13 @@
+// console.log("Its 34th Video")
+
+const changBgRed = ()=>{
+    document.body.firstElementChild.style.background = 'lightblue'
+}
+
+
+let b = document.body
+// console.log("Its document body", b)
+console.log("FirstChild of Body", b.firstChild)
+console.log("FirstElementChild of Body", b.firstElementChild)
+
+
