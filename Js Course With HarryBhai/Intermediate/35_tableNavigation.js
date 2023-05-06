1dc055a54a8b26075c9e4f17dfe91c684df2843b
Nilamjr <nilamrj07@gmail.com>
2023-05-06 06:20:33 +0530

    table navigation

---
+++ Js Course With HarryBhai/Intermediate/35_tableNavigation.js
@@ -0,0 +1,9 @@
+// let t = document.body.firstChild
+let t = document.body.firstElementChild
+// let t = document.body.firstElementChild.firstElementChild
+
+console.log(t);
+console.log(t.tHead);
+console.log(t.rows);
+console.log(t.tBodies);
+console.log(t.rows)
