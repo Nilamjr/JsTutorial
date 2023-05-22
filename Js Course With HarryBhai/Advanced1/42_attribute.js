9070b16d1a30b1c987440161f1d0e0a130eda859
Nilamjr <nilamrj07@gmail.com>
2023-05-22 06:46:54 +0530

    javascript  attributes

---
+++ Js Course With HarryBhai/Advanced1/42_attribute.js
@@ -0,0 +1,8 @@
+let a = document.getElementById('krishna');
+let b = a.getAttribute('class');
+console.log("why this is not showing",b);
+let c = a.hasAttribute('style');
+console.log(c);
+let d = a.hasAttribute('class');
+console.log(d);
+// a.setAttribute("hidden", "true");
